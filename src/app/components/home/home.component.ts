c0fed572747b92e4763b3e2676f5c039c9e0f7e3
tayfurparmak <tayfur-parmak-1907@hotmail.com>
2023-11-29 22:24:27 +0300

    Projects listed.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,9 +4,14 @@ import { ProjectService } from '../../services/project.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  constructor(public projectService:ProjectService){}
+  constructor(public projectService: ProjectService) {}
 
+  public remove(id: number) {
+    if (confirm('Are you sure?')) {
+      this.projectService.Remove(id);
+    }
+  }
 }
